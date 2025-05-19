31eeeaac5aecf005c28db418765868f67b4d6da3
Alexander Yavtuhovich <yavtik@gmail.com>
2025-05-19 15:30:50 +0300

    responsive page structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,19 @@
-import { Box, Button } from '@chakra-ui/react'
+import { Grid, GridItem, Stack } from '@chakra-ui/react'
 import './App.css'
-import { ColorModeButton } from './components/ui/color-mode'
-import { Tooltip } from './components/ui/tooltip'
-import { Toaster, toaster } from './components/ui/toaster'
 function App() {
 
   return (
     <>
-      <ColorModeButton>
-      </ColorModeButton>
-      <Toaster></Toaster>
-      <Tooltip content="box description" showArrow contentProps={{ backgroundColor: "blue" }}>
-        <Box>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium necessitatibus facilis vitae quia temporibus cum. Soluta, rem deserunt dolorem iure a, dignissimos animi harum culpa sint unde labore, accusamus cupiditate.
-        </Box>
-      </Tooltip>
-      <Button onClick={() => toaster.create({
-        description: "File not saved",
-        type: "warning",
-        action: {
-          label: "X",
-          onClick: () => { }
-        },
-
-      })}>Save file</Button>
+      <Grid gridTemplateAreas={{
+        base: `'nav' 'main'`,
+        md: `'nav nav' 'aside main'`
+      }}>
+        <GridItem area="nav" bg="gold">NAV</GridItem>
+        <Stack hideBelow={"md"}>
+          <GridItem area="aside" bg="coral">ASIDE</GridItem>
+        </Stack>
+        <GridItem area="main" bg="dodgerblue">MAIN</GridItem>
+      </Grid>
     </>
   )
 }
